daa2cb3c898aa296b6acf2cbaee859130dd4d1eb
Manuel Villegas <manuelv0703@gmail.com>
2024-07-23 13:56:39 -0500

    Updated ContainsDuplicates

---
+++ Arrays-and-Hashing/ContainsDuplicates/C++/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) 
+    {
+        unordered_set <int> hashset;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(hashset.find(nums[i]) != hashset.end())
+                return true;
+            hashset.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
