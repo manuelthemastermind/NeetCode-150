11d32c8092669094b92ec3b7a8b1161a25b8f958
Manuel Villegas <manuelv0703@gmail.com>
2024-07-23 16:30:19 -0500

    Updated files for their respected problem

---
+++ Arrays-and-Hashing/TwoSum/C++/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+     unordered_map <int, int> hashmap;
+
+     for(int i = 0; i < nums.size(); i++)
+     {
+        int diff = target - nums[i];
+
+        if(hashmap.count(diff))
+            return {hashmap[diff], i};
+        hashmap[nums[i]] = i;
+     }
+     return {};   
+    }
+};
