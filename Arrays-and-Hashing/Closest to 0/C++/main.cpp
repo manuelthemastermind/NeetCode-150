d3898e390664cdaafc3124b450c48e668bb83b61
Manuel Villegas <manuelv0703@gmail.com>
2024-07-24 20:18:27 -0500

    Update files and uploaded code for Closest to 0 LeetCode Problem

---
+++ Arrays-and-Hashing/Closest to 0/C++/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+class Solution {
+public:
+    int findClosestNumber(std::vector<int>& nums) 
+    {
+        int closest = nums[0];
+
+        for(int num : nums)
+        {
+            if(abs(num) < abs(closest))
+                closest = num;
+            if(abs(num) == abs(closest))
+                closest = std::max(closest, num);
+        }
+        return closest;
+    }
+};
---
+++ Arrays-and-Hashing/Closest to 0/Python/main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        
+        for num in nums:
+            if(abs(num) < abs(closest)):
+                closest = num
+            if(abs(num) == abs(closest)):
+                closest = max(closest, num)
+        
+        return closest
