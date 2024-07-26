f0d9f80f50dac374da519a4c6f549e9b64978785
Manuel Villegas <manuelv0703@gmail.com>
2024-07-25 23:14:52 -0500

    Uploaded C++ for Merge Strings

---
+++ Arrays-and-Hashing/Merging Strings Alternatively/C++/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Solution
+{
+public:
+     std::string mergeAlternately(std::string word1, std::string word2)
+     {
+        std::string result = "";
+
+        for(int index = 0; index < word1.length() || index < word2.length(); index++)
+        {
+            if(index < word1.length())
+                result += word1[index];
+            if(index < word2.length())
+                result += word2[index];
+        }
+
+        return result;
+
+     }
+
+
+};
