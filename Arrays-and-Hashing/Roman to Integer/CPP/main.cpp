480c1fbca8ce68e830bf940656a76713f317eb51
Manuel Villegas <manuelv0703@gmail.com>
2024-07-31 00:17:29 -0500

    Uploaded cpp solution

---
+++ Arrays-and-Hashing/Roman to Integer/CPP/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(std::string s) 
+    {
+        std::unordered_map <char, int> hashmap;
+
+        hashmap['I'] = 1;
+        hashmap['V'] = 5;
+        hashmap['X'] = 10;
+        hashmap['L'] = 50;
+        hashmap['C'] = 100;
+        hashmap['D'] = 500;
+        hashmap['M'] = 1000;
+
+        int result = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(hashmap[s[i]] < hashmap[s[i+1]])
+                result -= hashmap[s[i]];
+            else
+                result += hashmap[s[i]];
+        }
+
+        return result;
+    }
+};
